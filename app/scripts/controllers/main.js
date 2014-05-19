2dec180b3e0b0370e8f9f0d9c57532fa357a49a0
Andrew Raynal <andrewraynal@Andrews-MacBook-Air.local>
2014-05-19 18:03:17 -0400

    first

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularcartApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
